794c6f3c386c82e419f73f512a1a069bf30ab558
Nebojša Kamber <kamber@nebojsa.in.rs>
2015-12-01 23:17:34 +0100

    Add Map activity - https://trello.com/c/PrRKWI5v

--- app/src/main/java/com/technics/trnqlchallenge/MainActivity.java
+++ app/src/main/java/com/technics/trnqlchallenge/MainActivity.java
@@ -2,6 +2,7 @@ package com.technics.trnqlchallenge;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.trnql.zen.core.AppData;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppData.startAllServices(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        AppData.stopAllServices(getApplication());
+        super.onDestroy();
     }
 }
