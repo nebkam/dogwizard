a5f8bdf08a4a196ad139d72fac1d438c8dd437a9
Nebojša Kamber <kamber@nebojsa.in.rs>
2015-12-01 23:50:34 +0100

    Listen to lat/lng change - https://trello.com/c/PrRKWI5v

--- app/src/main/java/com/technics/trnqlchallenge/MainActivity.java
+++ app/src/main/java/com/technics/trnqlchallenge/MainActivity.java
@@ -1,7 +1,8 @@
 package com.technics.trnqlchallenge;
 
-import com.trnql.smart.base.SmartCompatActivity;
 import android.os.Bundle;
+import com.trnql.smart.base.SmartCompatActivity;
+import com.trnql.smart.location.LocationEntry;
 import com.trnql.zen.core.AppData;
 
 public class MainActivity extends SmartCompatActivity {
@@ -12,6 +13,13 @@ public class MainActivity extends SmartCompatActivity {
         setContentView(R.layout.activity_main);
         getAppData().setApiKey("a260b0e4-74f9-4bc8-bc2c-a73c4861ea82");
         AppData.startAllServices(this);
+    }
+
+    @Override
+    protected void smartLatLngChange(LocationEntry value) {
+//        location_latitude.setText(String.format("Latitude:   %s", String.valueOf(value.getLatitude())));
+//        location_longitude.setText(String.format("Longitude:   %s", String.valueOf(value.getLongitude())));
+//        location_accuracy.setText(String.format("Accuracy:   %s", String.valueOf(value.getAccuracy())));
     }
 
     @Override
