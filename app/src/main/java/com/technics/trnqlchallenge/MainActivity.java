1c15584d4ec857798935f02c0f3af12b3c1a8fd9
Nebojša Kamber <kamber@nebojsa.in.rs>
2015-12-01 22:30:47 +0100

    Use Trnql sample as a starting point - https://trello.com/c/tmUns5CY

--- app/src/main/java/com/technics/trnqlchallenge/MainActivity.java
+++ app/src/main/java/com/technics/trnqlchallenge/MainActivity.java
@@ -1,17 +1,13 @@
 package com.technics.trnqlchallenge;
 
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.trnql.smart.base.SmartCompatActivity;
-import com.trnql.zen.core.db.DbKVP;
-
-public class MainActivity extends SmartCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getAppData().setApiKey("a260b0e4-74f9-4bc8-bc2c-a73c4861ea82");
-        DbKVP myDB = getDBManager().getDB_KVP(R.id.db_kvp_trnql);
     }
 }
