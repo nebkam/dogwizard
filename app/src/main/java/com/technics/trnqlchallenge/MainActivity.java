c69af6a6788820c1eedfc3302cb8308a095aabb4
Nebojša Kamber <kamber@nebojsa.in.rs>
2015-12-01 21:03:56 +0100

    TRNQL SDK - https://trello.com/c/tmUns5CY

--- app/src/main/java/com/technics/trnqlchallenge/MainActivity.java
+++ app/src/main/java/com/technics/trnqlchallenge/MainActivity.java
@@ -1,13 +1,17 @@
 package com.technics.trnqlchallenge;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.trnql.smart.base.SmartCompatActivity;
+import com.trnql.zen.core.db.DbKVP;
+
+public class MainActivity extends SmartCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getAppData().setApiKey("a260b0e4-74f9-4bc8-bc2c-a73c4861ea82");
+        DbKVP myDB = getDBManager().getDB_KVP(R.id.db_kvp_trnql);
     }
 }
