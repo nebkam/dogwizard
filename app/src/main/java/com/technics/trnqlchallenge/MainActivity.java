fda27ffec45c47ec1b84591d098d956a9277845c
Nebojša Kamber <kamber@nebojsa.in.rs>
2015-12-01 23:38:40 +0100

    Init Trnql SmartActivity - https://trello.com/c/PrRKWI5v

--- app/src/main/java/com/technics/trnqlchallenge/MainActivity.java
+++ app/src/main/java/com/technics/trnqlchallenge/MainActivity.java
@@ -1,15 +1,16 @@
 package com.technics.trnqlchallenge;
 
-import android.support.v7.app.AppCompatActivity;
+import com.trnql.smart.base.SmartCompatActivity;
 import android.os.Bundle;
 import com.trnql.zen.core.AppData;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends SmartCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getAppData().setApiKey("a260b0e4-74f9-4bc8-bc2c-a73c4861ea82");
         AppData.startAllServices(this);
     }
 
