bd2783a6cd78019fb4f6a85b0aaa08e7119a15d8
Nebojša Kamber <kamber@nebojsa.in.rs>
2015-12-02 20:55:13 +0100

    Reset, again

--- app/src/main/java/com/technics/trnqlchallenge/MainActivity.java
+++ app/src/main/java/com/technics/trnqlchallenge/MainActivity.java
@@ -1,30 +1,13 @@
 package com.technics.trnqlchallenge;
 
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import com.trnql.smart.base.SmartCompatActivity;
-import com.trnql.smart.location.LocationEntry;
-import com.trnql.zen.core.AppData;
 
-public class MainActivity extends SmartCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getAppData().setApiKey("a260b0e4-74f9-4bc8-bc2c-a73c4861ea82");
-        AppData.startAllServices(this);
-    }
-
-    @Override
-    protected void smartLatLngChange(LocationEntry value) {
-//        location_latitude.setText(String.format("Latitude:   %s", String.valueOf(value.getLatitude())));
-//        location_longitude.setText(String.format("Longitude:   %s", String.valueOf(value.getLongitude())));
-//        location_accuracy.setText(String.format("Accuracy:   %s", String.valueOf(value.getAccuracy())));
-    }
-
-    @Override
-    protected void onDestroy() {
-        AppData.stopAllServices(getApplication());
-        super.onDestroy();
     }
 }
