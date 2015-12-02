574d7cdb066a9ddf09d6a8c1ff941091958e442d
Nebojša Kamber <kamber@nebojsa.in.rs>
2015-12-02 21:27:02 +0100

    Add TRNQL SDK that does not crash, yay - https://trello.com/c/GLprpo2H

--- app/src/main/java/com/technics/trnqlchallenge/MainActivity.java
+++ app/src/main/java/com/technics/trnqlchallenge/MainActivity.java
@@ -1,13 +1,15 @@
 package com.technics.trnqlchallenge;
 
-import android.support.v7.app.AppCompatActivity;
+import com.trnql.smart.base.SmartCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends SmartCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getAppData().setApiKey("a260b0e4-74f9-4bc8-bc2c-a73c4861ea82");
     }
 }
