4b158f75dc2a22f8ae1c0e89b18a62e8141c6379
Nebojša Kamber <kamber@nebojsa.in.rs>
2015-12-02 21:57:04 +0100

    Add Parse SDK - https://trello.com/c/hUCXgKyP

--- app/src/main/java/com/technics/trnqlchallenge/MainActivity.java
+++ app/src/main/java/com/technics/trnqlchallenge/MainActivity.java
@@ -1,5 +1,7 @@
 package com.technics.trnqlchallenge;
 
+import com.parse.Parse;
+import com.parse.ParseObject;
 import com.trnql.smart.base.SmartCompatActivity;
 import android.os.Bundle;
 
@@ -9,7 +11,14 @@ public class MainActivity extends SmartCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //Init TRNQL SDK
         getAppData().setApiKey("a260b0e4-74f9-4bc8-bc2c-a73c4861ea82");
+        //Init Parse SDK
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, "HjP85guTMScAOKptkuVmXNY7OuHxF0HbNEpWl2gD", "LDtwzrmTgBdQPICRLLdhLaNqUR4Q2vnLdzlUewAs");
+
+//        ParseObject testObject = new ParseObject("TestObject");
+//        testObject.put("foo", "bar");
+//        testObject.saveInBackground();
     }
 }
