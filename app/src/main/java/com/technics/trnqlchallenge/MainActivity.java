82bedfcbe4484700fd2aa68f9367462dbddff13c
Nebojša Kamber <kamber@nebojsa.in.rs>
2015-12-05 00:08:47 +0100

    DogDescriptionActivity as splash screen - https://trello.com/c/SzJobFLf

--- app/src/main/java/com/technics/trnqlchallenge/MainActivity.java
+++ app/src/main/java/com/technics/trnqlchallenge/MainActivity.java
@@ -5,6 +5,7 @@ import com.trnql.smart.base.SmartCompatActivity;
 import com.trnql.smart.location.LocationEntry;
 import com.trnql.smart.people.PersonEntry;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
@@ -19,6 +20,10 @@ public class MainActivity extends SmartCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (isFirstRun()) {
+            showSplash();
+        }
 
         //If we've got an ID from Parse, use it as an access token
         if (user.getObjectId() != null) {
@@ -44,6 +49,18 @@ public class MainActivity extends SmartCompatActivity {
         Intent intent = new Intent(MainActivity.this,MapsActivity.class);
         intent.putExtra("com.technics.trnqlchallenge.LAT", latitude);
         intent.putExtra("com.technics.trnqlchallenge.LONG", longitude);
+        startActivity(intent);
+    }
+
+    private boolean isFirstRun() {
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        return preferences.getBoolean("firstRun",true);
+    }
+
+    private void showSplash(){
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        preferences.edit().putBoolean("firstRun",false).apply();
+        Intent intent = new Intent(MainActivity.this,DogDescriptionActivity.class);
         startActivity(intent);
     }
 }
