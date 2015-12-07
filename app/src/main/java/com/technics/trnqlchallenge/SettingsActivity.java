21d12171b90d6bc7c0d56a410330600409f8185a
Nebojša Kamber <kamber@nebojsa.in.rs>
2015-12-07 14:00:55 +0100

    https://trello.com/c/Aa57XF6y

--- app/src/main/java/com/technics/trnqlchallenge/SettingsActivity.java
+++ app/src/main/java/com/technics/trnqlchallenge/SettingsActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 
+import com.parse.ParseUser;
+
 public class SettingsActivity extends AppCompatActivity {
 
     @Override
@@ -12,5 +14,31 @@ public class SettingsActivity extends AppCompatActivity {
         setTitle(R.string.settings_title);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Switch contactSwitch = (Switch) findViewById(R.id.switch_contact);
+        Switch dogWalkSwitch = (Switch) findViewById(R.id.switch_dog_walk);
+
+        //Prepopulate
+        ParseUser user = ParseUser.getCurrentUser();
+        contactSwitch.setChecked(user.getBoolean("contact"));
+        dogWalkSwitch.setChecked(user.getBoolean("dogWalkReminder"));
+
+        //Listeners
+        contactSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                ParseUser user = ParseUser.getCurrentUser();
+                user.put("contact", isChecked);
+                user.saveInBackground();
+            }
+        });
+        dogWalkSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                ParseUser user = ParseUser.getCurrentUser();
+                user.put("dogWalkReminder",isChecked);
+                user.saveInBackground();
+            }
+        });
     }
 }
