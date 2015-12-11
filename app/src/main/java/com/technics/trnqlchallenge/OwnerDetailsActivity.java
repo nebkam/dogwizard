480af3c79bf27601f99e4901ce25eb255451370b
Nebojša Kamber <kamber@nebojsa.in.rs>
2015-12-11 19:30:24 +0100

    Fetch owner from Parse - https://trello.com/c/ZmZzLdM5

--- app/src/main/java/com/technics/trnqlchallenge/OwnerDetailsActivity.java
+++ app/src/main/java/com/technics/trnqlchallenge/OwnerDetailsActivity.java
@@ -5,14 +5,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
 public class OwnerDetailsActivity extends AppCompatActivity {
+    private TextView dogName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_owner_details);
+
         Intent intent = getIntent();
-        TextView textView = (TextView)findViewById(R.id.textView4);
-        textView.setText(intent.getStringExtra("userToken"));
+        String userToken = intent.getStringExtra("userToken");
+        dogName = (TextView)findViewById(R.id.dogName);
+
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        query.getInBackground(userToken, new GetCallback<ParseUser>() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e == null && user != null && user.getString("dogName") != null) {
+                    dogName.setText(user.getString("dogName"));
+                } else {
+                    dogName.setText("Unknown");
+                }
+            }
+        });
     }
 }
