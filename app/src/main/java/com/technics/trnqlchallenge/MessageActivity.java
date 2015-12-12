d7d83052b96e6220e302b275ec86102c7b96904c
Nebojša Kamber <kamber@nebojsa.in.rs>
2015-12-12 18:37:51 +0100

    Validate

--- app/src/main/java/com/technics/trnqlchallenge/MessageActivity.java
+++ app/src/main/java/com/technics/trnqlchallenge/MessageActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.parse.ParseObject;
 
@@ -17,8 +18,10 @@ public class MessageActivity extends AppCompatActivity {
 
     public void sendMessage(View view) {
         EditText body = (EditText) findViewById(R.id.messageBody);
-
-        ParseObject message = new ParseObject("Message");
-
+        if (body.getText().toString().isEmpty()) {
+            Toast.makeText(getApplicationContext(),R.string.err_message,Toast.LENGTH_SHORT).show();
+            return;
+        }
+//        ParseObject message = new ParseObject("Message");
     }
 }
