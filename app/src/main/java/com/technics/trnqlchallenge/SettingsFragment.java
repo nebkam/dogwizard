26a604c6fc4c4879986d11b74285d3399265be95
Nebojša Kamber <kamber@nebojsa.in.rs>
2015-12-13 00:35:49 +0100

    Edit dog name inline - https://trello.com/c/ENSOW0Dh

--- app/src/main/java/com/technics/trnqlchallenge/SettingsFragment.java
+++ app/src/main/java/com/technics/trnqlchallenge/SettingsFragment.java
@@ -1,13 +1,47 @@
 package com.technics.trnqlchallenge;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 
+import com.parse.ParseUser;
+
 public class SettingsFragment extends PreferenceFragment {
+    private SharedPreferences.OnSharedPreferenceChangeListener listener =
+            new SharedPreferences.OnSharedPreferenceChangeListener() {
+                public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+                    // listener implementation
+                    if (key.equals("pref_name")) {
+                        ParseUser user = ParseUser.getCurrentUser();
+                        user.put("dogName",prefs.getString("pref_name","Anonymous"));
+                        user.saveInBackground();
+                    }
+                }
+            };
+
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
+        // Register a listener for persisting to Parse
+        getPreferenceScreen()
+                .getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(listener);
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceScreen()
+                .getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(listener);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getPreferenceScreen()
+                .getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(listener);
     }
 }
