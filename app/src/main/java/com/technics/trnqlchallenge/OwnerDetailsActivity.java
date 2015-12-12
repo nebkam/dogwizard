a87fbfb8c9561c3c6f6cfd55b9f0f52ed1ea012e
Nebojša Kamber <kamber@nebojsa.in.rs>
2015-12-12 10:53:16 +0100

    Started on photo rendering - https://trello.com/c/ZmZzLdM5

--- app/src/main/java/com/technics/trnqlchallenge/OwnerDetailsActivity.java
+++ app/src/main/java/com/technics/trnqlchallenge/OwnerDetailsActivity.java
@@ -1,8 +1,12 @@
 package com.technics.trnqlchallenge;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.parse.GetCallback;
@@ -13,6 +17,7 @@ import com.parse.ParseUser;
 public class OwnerDetailsActivity extends AppCompatActivity {
     private TextView dogName;
     private TextView dogBreed;
+//    private ImageView dogPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +28,12 @@ public class OwnerDetailsActivity extends AppCompatActivity {
         String userToken = intent.getStringExtra("userToken");
         dogName = (TextView)findViewById(R.id.dogName);
         dogBreed = (TextView)findViewById(R.id.dogBreed);
+//        dogPhoto = (ImageView)findViewById(R.id.dogPhoto);
 
         ParseQuery<ParseUser> query = ParseUser.getQuery();
         query.getInBackground(userToken, new GetCallback<ParseUser>() {
             @Override
             public void done(ParseUser user, ParseException e) {
-                dogName.setText("Anonymous");
-                dogBreed.setText("Unknown");
                 if (e == null && user != null) {
                     if (user.getString("dogName") != null) {
                         dogName.setText(user.getString("dogName"));
@@ -37,6 +41,11 @@ public class OwnerDetailsActivity extends AppCompatActivity {
                     if (user.getString("dogBreed") != null) {
                         dogBreed.setText(user.getString("dogBreed"));
                     }
+//                    if (user.getString("photo") != null) {
+//                        byte[] bytes = Base64.decode(user.getString("photo"),Base64.DEFAULT);
+//                        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+//                        dogPhoto.setImageBitmap(bitmap);
+//                    }
                 }
             }
         });
