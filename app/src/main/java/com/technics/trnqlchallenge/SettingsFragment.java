754f9ad17c416dbe468258491434f2d8eef58581
Nebojša Kamber <kamber@nebojsa.in.rs>
2015-12-13 10:42:02 +0100

    Preference value as a summary - https://trello.com/c/ENSOW0Dh

--- app/src/main/java/com/technics/trnqlchallenge/SettingsFragment.java
+++ app/src/main/java/com/technics/trnqlchallenge/SettingsFragment.java
@@ -2,18 +2,27 @@ package com.technics.trnqlchallenge;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 
 import com.parse.ParseUser;
 
 public class SettingsFragment extends PreferenceFragment {
+    public static final String KEY_PREF_DOG_NAME = "pref_name";
     private SharedPreferences.OnSharedPreferenceChangeListener listener =
             new SharedPreferences.OnSharedPreferenceChangeListener() {
                 public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
                     // listener implementation
-                    if (key.equals("pref_name")) {
+                    if (key.equals(KEY_PREF_DOG_NAME)) {
+                        String dogName = prefs.getString(KEY_PREF_DOG_NAME,"Anonymous");
+
+                        //Update UI
+                        Preference pref = findPreference(KEY_PREF_DOG_NAME);
+                        pref.setSummary(dogName);
+
+                        //Persist to Parse
                         ParseUser user = ParseUser.getCurrentUser();
-                        user.put("dogName",prefs.getString("pref_name","Anonymous"));
+                        user.put("dogName",dogName);
                         user.saveInBackground();
                     }
                 }
@@ -24,6 +33,11 @@ public class SettingsFragment extends PreferenceFragment {
         super.onCreate(savedInstanceState);
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
+
+        //Set values to summaries
+        Preference dogName = findPreference(KEY_PREF_DOG_NAME);
+        dogName.setSummary( dogName.getSharedPreferences().getString(KEY_PREF_DOG_NAME,"Anonymous") );
+
         // Register a listener for persisting to Parse
         getPreferenceScreen()
                 .getSharedPreferences()
