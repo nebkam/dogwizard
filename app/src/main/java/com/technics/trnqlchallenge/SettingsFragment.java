02b458e18a94262d1b8478be7eae9f9a1e11fade
Nebojša Kamber <kamber@nebojsa.in.rs>
2015-12-13 10:54:50 +0100

    Edit dog breed inline - https://trello.com/c/ENSOW0Dh

--- app/src/main/java/com/technics/trnqlchallenge/SettingsFragment.java
+++ app/src/main/java/com/technics/trnqlchallenge/SettingsFragment.java
@@ -9,6 +9,8 @@ import com.parse.ParseUser;
 
 public class SettingsFragment extends PreferenceFragment {
     public static final String KEY_PREF_DOG_NAME = "pref_name";
+    public static final String KEY_PREF_DOG_BREED = "pref_breed";
+
     private SharedPreferences.OnSharedPreferenceChangeListener listener =
             new SharedPreferences.OnSharedPreferenceChangeListener() {
                 public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
@@ -25,6 +27,18 @@ public class SettingsFragment extends PreferenceFragment {
                         user.put("dogName",dogName);
                         user.saveInBackground();
                     }
+                    if (key.equals(KEY_PREF_DOG_BREED)) {
+                        String dogBreed = prefs.getString(KEY_PREF_DOG_BREED,"Unknown");
+
+                        //Update UI
+                        Preference pref = findPreference(KEY_PREF_DOG_BREED);
+                        pref.setSummary(dogBreed);
+
+                        //Persist to Parse
+                        ParseUser user = ParseUser.getCurrentUser();
+                        user.put("dogBreed",dogBreed);
+                        user.saveInBackground();
+                    }
                 }
             };
 
@@ -37,6 +51,8 @@ public class SettingsFragment extends PreferenceFragment {
         //Set values to summaries
         Preference dogName = findPreference(KEY_PREF_DOG_NAME);
         dogName.setSummary( dogName.getSharedPreferences().getString(KEY_PREF_DOG_NAME,"Anonymous") );
+        Preference dogBreed = findPreference(KEY_PREF_DOG_BREED);
+        dogBreed.setSummary( dogBreed.getSharedPreferences().getString(KEY_PREF_DOG_BREED,"Unknown") );
 
         // Register a listener for persisting to Parse
         getPreferenceScreen()
