cd25cfbcdee9866a0f55462ab1dff2ceaf836998
Nebojša Kamber <kamber@nebojsa.in.rs>
2015-12-13 11:19:54 +0100

    Started on dog photo editing - https://trello.com/c/ENSOW0Dh

---
+++ app/src/main/java/com/technics/trnqlchallenge/DogPhotoActivity.java
@@ -0,0 +1,13 @@
+package com.technics.trnqlchallenge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DogPhotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dog_photo);
+    }
+}
