29da0acb034bd6e7f45a6e48b7a41705d75a03ae
Nebojša Kamber <kamber@nebojsa.in.rs>
2015-12-13 12:01:31 +0100

    Dog photo editing layout - https://trello.com/c/ENSOW0Dh

--- app/src/main/java/com/technics/trnqlchallenge/DogPhotoActivity.java
+++ app/src/main/java/com/technics/trnqlchallenge/DogPhotoActivity.java
@@ -1,7 +1,13 @@
 package com.technics.trnqlchallenge;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.widget.ImageView;
+
+import com.parse.ParseUser;
 
 public class DogPhotoActivity extends AppCompatActivity {
 
@@ -9,5 +15,13 @@ public class DogPhotoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dog_photo);
+
+        ParseUser user = ParseUser.getCurrentUser();
+        ImageView dogPhoto = (ImageView)findViewById(R.id.dogPhoto);
+        if (user.getString("photo") != null) {
+            byte[] bytes = Base64.decode(user.getString("photo"), Base64.DEFAULT);
+            Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+            dogPhoto.setImageBitmap(bitmap);
+        }
     }
 }
