d2eaa397fb470059194c609f038f81cc942d7a96
Nebojša Kamber <kamber@nebojsa.in.rs>
2015-12-14 19:59:38 +0100

    GeoPoint and feedback - https://trello.com/c/xGXh1Osb

--- app/src/main/java/com/technics/trnqlchallenge/SendAnnouncementActivity.java
+++ app/src/main/java/com/technics/trnqlchallenge/SendAnnouncementActivity.java
@@ -4,45 +4,59 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.parse.ParseException;
+import com.parse.ParseGeoPoint;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 public class SendAnnouncementActivity extends AppCompatActivity {
-    private ParseUser user = ParseUser.getCurrentUser();
     private Double latitude;
     private Double longitude;
-    private EditText message;
-    private ParseObject announcement;
+    private EditText announcementBody;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_announcement);
 
-//        Button announceButton = (Button) findViewById(R.id.announceButton);
-//        latitude = getIntent().getDoubleExtra("latitude",0.00);
-//        longitude = getIntent().getDoubleExtra("longitude",0.00);
-//        message = (EditText) findViewById(R.id.announceMessage);
-//        announcement = new ParseObject("Announcement");
-//
-//        announceButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                announcement.put("message", message.getText().toString());
-//                announcement.put("latitude", latitude);
-//                announcement.put("longitude", longitude);
-//                announcement.put("userToken", user.getObjectId());
-//                announcement.saveInBackground();
-//
-//                Intent intent = new Intent(SendAnnouncementActivity.this, OwnersNearbyActivity.class);
-//                intent.putExtra("latitude", latitude);
-//                intent.putExtra("longitude", longitude);
-//                startActivity(intent);
-//            }
-//        });
+        latitude = getIntent().getDoubleExtra("latitude",0.00);
+        longitude = getIntent().getDoubleExtra("longitude",0.00);
+        announcementBody = (EditText) findViewById(R.id.announcementBody);
+    }
 
+    public void sendAnnouncement(View view) {
+        if (latitude == 0.00 || longitude == 0.00) {
+            Toast.makeText(getApplicationContext(), R.string.err_location, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (announcementBody.getText().toString().isEmpty()) {
+            Toast.makeText(getApplicationContext(), R.string.err_message, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        ParseObject announcement = new ParseObject("Announcement");
+        ParseGeoPoint point = new ParseGeoPoint(latitude,longitude);
+        ParseUser user = ParseUser.getCurrentUser();
+        announcement.put("body", announcementBody.getText().toString());
+        announcement.put("location",point);
+        announcement.put("from",user.getObjectId());
+        announcement.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Toast.makeText(getApplicationContext(), R.string.announcement_sent, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        Intent intent = new Intent(SendAnnouncementActivity.this, OwnersNearbyActivity.class);
+        intent.putExtra("latitude", latitude);
+        intent.putExtra("longitude", longitude);
+        startActivity(intent);
     }
 }
