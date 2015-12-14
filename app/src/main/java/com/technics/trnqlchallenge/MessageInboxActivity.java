75657f9cb2ee6ad272864dc8049656416a92c23d
Pitchwais <sedlarb@hotmail.com>
2015-12-14 23:18:24 +0100

    View message activity

--- app/src/main/java/com/technics/trnqlchallenge/MessageInboxActivity.java
+++ app/src/main/java/com/technics/trnqlchallenge/MessageInboxActivity.java
@@ -1,9 +1,13 @@
 package com.technics.trnqlchallenge;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.parse.Parse;
 import com.parse.ParseObject;
 import com.parse.ParseQueryAdapter;
 
@@ -14,9 +18,24 @@ public class MessageInboxActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message_inbox);
 
-        ParseQueryAdapter<ParseObject> adapter = new ParseQueryAdapter<ParseObject>(this,"Message");
+        final ParseQueryAdapter<ParseObject> adapter = new ParseQueryAdapter<ParseObject>(this,"Message");
         adapter.setTextKey("body");
+
         ListView messageList = (ListView)findViewById(R.id.messageList);
         messageList.setAdapter(adapter);
+
+        messageList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                ParseUser friend = mAdapter.getItem(position);
+                  ParseObject message = adapter.getItem(position);
+
+                Intent intent = new Intent(MessageInboxActivity.this,ViewMessageActivity.class);
+                intent.putExtra("body", message.getString("body"));
+                intent.putExtra("from", message.getString("from"));
+                startActivity(intent);
+            }
+        });
     }
 }
