6e54a176c8201c7b990fd9cb6af2b63608ce8f48
Nebojša Kamber <kamber@nebojsa.in.rs>
2015-12-20 12:05:50 +0100

    Minor

--- app/src/main/java/com/technics/trnqlchallenge/DogFriendlyPlacesActivity.java
+++ app/src/main/java/com/technics/trnqlchallenge/DogFriendlyPlacesActivity.java
@@ -3,10 +3,8 @@ package com.technics.trnqlchallenge;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -15,14 +13,11 @@ import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.parse.FindCallback;
-import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseGeoPoint;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
-import com.parse.ParseQueryAdapter;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -48,14 +43,13 @@ public class DogFriendlyPlacesActivity extends AppCompatActivity implements OnMa
 
         ParseGeoPoint point = new ParseGeoPoint(latitude,longitude);
 
-        ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("FriendlyPlace");
+        ParseQuery<ParseObject> query = new ParseQuery<>("FriendlyPlace");
         query.whereWithinKilometers("location", point, 8.00).orderByDescending("createdAt");
         query.findInBackground(new FindCallback<ParseObject>() {
             public void done(List<ParseObject> objects, ParseException e) {
                 if (e == null) {
                     if (!objects.isEmpty()){
                         for (int i=0; i<objects.size(); i++) {
-//                            mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(boundsBuilder.build(), 30));
                             ParseObject object = objects.get(i);
 
                             String name = object.getString("name");
@@ -73,28 +67,24 @@ public class DogFriendlyPlacesActivity extends AppCompatActivity implements OnMa
 
                             DogFriendlyPlace dogFriendlyPlace = new DogFriendlyPlace(name,address,phone,website);
                             dogFriendlyPlaceHashMap.put(marker.getId(), dogFriendlyPlace);
-
-                            mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-                                @Override
-                                public boolean onMarkerClick(Marker marker) {
-                                    DogFriendlyPlace markedPlace = dogFriendlyPlaceHashMap.get(marker.getId());
-                                    Intent intent = new Intent(DogFriendlyPlacesActivity.this, DogFriendlyPlaceDetailsActivity.class);
-                                    intent.putExtra("name", markedPlace.name);
-                                    intent.putExtra("city", markedPlace.address);
-                                    intent.putExtra("phone", markedPlace.phone);
-                                    intent.putExtra("website", markedPlace.website);
-                                    startActivity(intent);
-                                    return true;
-                                }
-                            });
                         }
+                        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+                            @Override
+                            public boolean onMarkerClick(Marker marker) {
+                                DogFriendlyPlace markedPlace = dogFriendlyPlaceHashMap.get(marker.getId());
+                                Intent intent = new Intent(DogFriendlyPlacesActivity.this, DogFriendlyPlaceDetailsActivity.class);
+                                intent.putExtra("name", markedPlace.name);
+                                intent.putExtra("city", markedPlace.address);
+                                intent.putExtra("phone", markedPlace.phone);
+                                intent.putExtra("website", markedPlace.website);
+                                startActivity(intent);
+                                return true;
+                            }
+                        });
                     }
                     else {
                         Toast.makeText(getApplicationContext(), R.string.no_friendly_places_nearby, Toast.LENGTH_LONG).show();
                     }
-
-                } else {
-                    System.out.println(e.getMessage());
                 }
             }
         });
