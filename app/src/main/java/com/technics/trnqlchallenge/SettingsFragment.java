bc48251bd594392bcd4e878ce0cbe5069c8edf5b
Pitchwais <sedlarb@hotmail.com>
2015-12-23 21:29:08 +0100

    Settings .

--- app/src/main/java/com/technics/trnqlchallenge/SettingsFragment.java
+++ app/src/main/java/com/technics/trnqlchallenge/SettingsFragment.java
@@ -10,6 +10,7 @@ import com.parse.ParseUser;
 public class SettingsFragment extends PreferenceFragment {
     public static final String KEY_PREF_DOG_NAME = "pref_name";
     public static final String KEY_PREF_DOG_BREED = "pref_breed";
+    public static final String KEY_PREF_DOG_SEX = "pref_sex";
 
     private SharedPreferences.OnSharedPreferenceChangeListener listener =
             new SharedPreferences.OnSharedPreferenceChangeListener() {
@@ -39,6 +40,16 @@ public class SettingsFragment extends PreferenceFragment {
                         user.put("dogBreed",dogBreed);
                         user.saveInBackground();
                     }
+                    if (key.equals(KEY_PREF_DOG_SEX)) {
+                        String dogSex = prefs.getString(KEY_PREF_DOG_SEX, "Unknown");
+                        Preference pref = findPreference(KEY_PREF_DOG_SEX);
+                        pref.setSummary(dogSex);
+
+                        //Persist to Parse
+                        ParseUser user = ParseUser.getCurrentUser();
+                        user.put("dogSex",dogSex);
+                        user.saveInBackground();
+                    }
                 }
             };
 
@@ -50,9 +61,12 @@ public class SettingsFragment extends PreferenceFragment {
 
         //Set values to summaries
         Preference dogName = findPreference(KEY_PREF_DOG_NAME);
-        dogName.setSummary( dogName.getSharedPreferences().getString(KEY_PREF_DOG_NAME,"Anonymous") );
+        dogName.setSummary(dogName.getSharedPreferences().getString(KEY_PREF_DOG_NAME, "Anonymous"));
         Preference dogBreed = findPreference(KEY_PREF_DOG_BREED);
         dogBreed.setSummary( dogBreed.getSharedPreferences().getString(KEY_PREF_DOG_BREED,"Unknown") );
+        Preference dogSex = findPreference(KEY_PREF_DOG_SEX);
+        dogSex.setSummary( dogSex.getSharedPreferences().getString(KEY_PREF_DOG_SEX,"Unknown") );
+
 
         // Register a listener for persisting to Parse
         getPreferenceScreen()
