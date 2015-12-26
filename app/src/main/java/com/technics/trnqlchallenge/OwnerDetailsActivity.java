fb8092fcc1b6edfe91bae9ebb4c871cccecc8c69
Nebojša Kamber <kamber@nebojsa.in.rs>
2015-12-26 14:17:56 +0100

    Owner details

--- app/src/main/java/com/technics/trnqlchallenge/OwnerDetailsActivity.java
+++ app/src/main/java/com/technics/trnqlchallenge/OwnerDetailsActivity.java
@@ -5,14 +5,15 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.parse.GetCallback;
+import com.parse.GetDataCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
@@ -21,50 +22,55 @@ public class OwnerDetailsActivity extends AppCompatActivity {
     private TextView dogName;
     private TextView dogBreed;
     private ImageView dogPhoto;
-    private Button contactBtn;
+    private Button contact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_owner_details);
 
-//        Intent intent = getIntent();
-//        userToken = intent.getStringExtra("userToken");
-//        dogName = (TextView)findViewById(R.id.dogName);
-//        dogBreed = (TextView)findViewById(R.id.dogBreed);
-//        dogPhoto = (ImageView)findViewById(R.id.dogPhoto);
-//        contactBtn = (Button)findViewById(R.id.btn_contact);
-//
-//        ParseQuery<ParseUser> query = ParseUser.getQuery();
-//        query.getInBackground(userToken, new GetCallback<ParseUser>() {
-//            @Override
-//            public void done(ParseUser user, ParseException e) {
-//                if (e == null && user != null) {
-//                    if (user.getString("dogName") != null) {
-//                        dogName.setText(user.getString("dogName"));
-//                    }
-//                    if (user.getString("dogBreed") != null) {
-//                        dogBreed.setText(user.getString("dogBreed"));
-//                    }
-//                    if (user.getString("photo") != null) {
-//                        byte[] bytes = Base64.decode(user.getString("photo"),Base64.DEFAULT);
-//                        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes,0,bytes.length);
-//                        dogPhoto.setImageBitmap(bitmap);
-//                    }
-//                    if (user.getBoolean("contact")) {
-//                        contactBtn.setVisibility(View.VISIBLE);
-//                    }
-//                    else {
-//                        contactBtn.setVisibility(View.GONE);
-//                    }
-//                }
-//            }
-//        });
+        Intent intent = getIntent();
+        userToken = intent.getStringExtra("userToken");
+        dogName = (TextView)findViewById(R.id.dogName);
+        dogBreed = (TextView)findViewById(R.id.dogBreed);
+        dogPhoto = (ImageView)findViewById(R.id.dogPhoto);
+        contact = (Button)findViewById(R.id.btn_contact);
+
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        query.getInBackground(userToken, new GetCallback<ParseUser>() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e == null && user != null) {
+                    if (user.getString("dogName") != null) {
+                        dogName.setText(user.getString("dogName"));
+                    }
+                    if (user.getString("dogBreed") != null
+                            && !user.getString("dogBreed").equals("Unknown")) {
+                        dogBreed.setText(user.getString("dogBreed"));
+                    }
+                    ParseFile file = user.getParseFile("photoFile");
+                    if (file != null) {
+                        file.getDataInBackground(new GetDataCallback() {
+                            @Override
+                            public void done(byte[] bytes, ParseException e) {
+                                if (e == null) {
+                                    Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                                    dogPhoto.setImageBitmap(bitmap);
+                                }
+                            }
+                        });
+                    }
+                    if (user.getBoolean("contact")) {
+                        contact.setVisibility(View.VISIBLE);
+                    }
+                }
+            }
+        });
     }
 
-//    public void contact(View view) {
-//        Intent intent = new Intent(OwnerDetailsActivity.this,MessageActivity.class);
-//        intent.putExtra("to",userToken);
-//        startActivity(intent);
-//    }
+    public void contact(View view) {
+        Intent intent = new Intent(OwnerDetailsActivity.this,MessageActivity.class);
+        intent.putExtra("to",userToken);
+        startActivity(intent);
+    }
 }
