7e9a872d00d3af41b93382d0cfbcb477a68baad5
Nebojša Kamber <kamber@nebojsa.in.rs>
2015-12-27 23:11:58 +0100

    Save dog name

--- app/src/main/java/com/technics/trnqlchallenge/SetupNameSexFragment.java
+++ app/src/main/java/com/technics/trnqlchallenge/SetupNameSexFragment.java
@@ -5,11 +5,29 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
-public class SetupNameSexFragment extends Fragment {
+import com.parse.ParseUser;
+
+public class SetupNameSexFragment extends Fragment  {
+    private EditText dogName;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_setup_2_name_sex, container, false);
+
+        dogName = (EditText)rootView.findViewById(R.id.dogName);
+        dogName.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                String updated = dogName.getText().toString();
+                if (!hasFocus && !updated.equals("")) {
+                    ParseUser user = ParseUser.getCurrentUser();
+                    user.put("dogName",updated);
+                    user.saveInBackground();
+                }
+            }
+        });
 
         return rootView;
     }
