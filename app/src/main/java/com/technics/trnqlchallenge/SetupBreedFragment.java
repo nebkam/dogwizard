d46170a1b9f923df42a801b795c749130e92f5bb
Nebojša Kamber <kamber@nebojsa.in.rs>
2015-12-27 21:22:42 +0100

    Started fragment #3

--- app/src/main/java/com/technics/trnqlchallenge/SetupBreedFragment.java
+++ app/src/main/java/com/technics/trnqlchallenge/SetupBreedFragment.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class SetupBreedFragment extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_setup_3_breed, container, false);
