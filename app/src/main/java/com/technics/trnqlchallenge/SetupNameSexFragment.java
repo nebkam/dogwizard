f8c82ff8ff3afff7bf2c24a16e2b015f5d5069de
Nebojša Kamber <kamber@nebojsa.in.rs>
2015-12-27 23:24:34 +0100

    Save dog sex

--- app/src/main/java/com/technics/trnqlchallenge/SetupNameSexFragment.java
+++ app/src/main/java/com/technics/trnqlchallenge/SetupNameSexFragment.java
@@ -6,11 +6,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 
 import com.parse.ParseUser;
 
 public class SetupNameSexFragment extends Fragment  {
     private EditText dogName;
+    private RadioGroup dogSex;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -24,6 +26,22 @@ public class SetupNameSexFragment extends Fragment  {
                 if (!hasFocus && !updated.equals("")) {
                     ParseUser user = ParseUser.getCurrentUser();
                     user.put("dogName",updated);
+                    user.saveInBackground();
+                }
+            }
+        });
+
+        dogSex = (RadioGroup) rootView.findViewById(R.id.dogSex);
+        dogSex.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId != -1) {
+                    ParseUser user = ParseUser.getCurrentUser();
+                    if (checkedId == R.id.male) {
+                        user.put("dogSex", "male");
+                    } else {
+                        user.put("dogSex", "female");
+                    }
                     user.saveInBackground();
                 }
             }
